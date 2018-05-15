812488fc2840480437b3a079dcdb1df873d465f3
MikkySnow <mikkysnowman@gmail.com>
2018-05-15 18:20:48 +0300

    Almost ready:

--- test/whitecoin.js
+++ test/whitecoin.js
@@ -1,9 +1,45 @@
-var WhiteCoin = artifacts.require("./WhiteCoin");
+var Whitecoin = artifacts.require("./Whitecoin.sol");
+const { should, EVMThrow } = require('./helpers');
 
-contract('WhiteCoin', function(accounts) {
-  it("should assert true", function(done) {
-    var WhiteCoin = WhiteCoin.deployed();
-    assert.isTrue(true);
-    done();
-  });
+contract('Whitecoin', function(accounts) {
+
+    let whitecoin;
+    let manager = accounts[0];
+
+    beforeEach('setup contract for each test', async function() {
+        whitecoin = await Whitecoin.new();
+    })
+
+    it('has a manager', async function() {
+        assert.equal(await whitecoin.isManager(manager), true)    
+    })
+
+    it('only one manager after creation', async function() {
+        assert.equal(await whitecoin.isManager(accounts[1]), false)    
+    })
+
+    it('manager can add minter', async function() {
+        await whitecoin.addMinter(accounts[1]);
+        assert.equal(await whitecoin.isMinter(accounts[1]), true)
+    })
+
+    it('minter can accept mint request', async function() {
+        let amount = 300000;
+        let address = accounts[2];
+        await whitecoin.addMinter(manager);
+        await whitecoin.mintingRequest(amount, address);
+    })
+
+    it('manager can accept mint request', async function() {
+        let amount = 30000;
+        let address = accounts[2];
+        await whitecoin.addMinter(manager);
+        await whitecoin.mintingRequest(amount, address);
+        await whitecoin.mintWhitecoin(1, 1);
+        assert.equal(await whitecoin.balanceOf(address), amount);
+    })
+
+    it('Should be rejected while non-minter adds minter', async function() {
+        whitecoin.addMinter(accounts[2], {from: accounts[2]}).should.be.rejectedWith(EVMThrow);
+    })
 });
